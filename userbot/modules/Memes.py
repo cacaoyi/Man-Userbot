4175359346fa7be46de1a8e76656921bd364242b
cacaoyi <96345977+cacaoyi@users.noreply.github.com>
2022-01-05 16:58:03 +0700

    Create Memes.py

---
+++ userbot/modules/Memes.py
@@ -0,0 +1,8 @@
+	@man_cmd(pattern="kalem(?: |$)(.*)")
+	async def _(event):
+	    xx = await edit_or_reply(event, f"**CIRCLE STELL KALEM**")
+	        sleep(2)
+	            await xx.edit("**STAY CALM & FIGHT**")
+	        sleep(2)
+	        	  await xx.edit("**☠️ SENGGOL BACOK ☠️**")
+	           
